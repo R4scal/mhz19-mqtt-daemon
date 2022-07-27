4bb9ae6c4345ecdcf2218326a5f5e7e65b401936
Murgeye <github@murgi.de>
2022-07-27 17:23:32 +0200

    Switch to PROTOCOL_TLS_CLIENT

--- mhz19-mqtt-daemon.py
+++ mhz19-mqtt-daemon.py
@@ -99,14 +99,13 @@ if reporting_mode in ['homeassistant-mqtt']:
     mqtt_client.on_publish = on_publish
 
     if config['MQTT'].getboolean('tls', False):
-        # According to the docs, setting PROTOCOL_SSLv23 "Selects the highest protocol version
-        # that both the client and server support. Despite the name, this option can select
-        # “TLS” protocols as well as “SSL”" - so this seems like a resonable default
         mqtt_client.tls_set(
             ca_certs=config['MQTT'].get('tls_ca_cert', None),
             keyfile=config['MQTT'].get('tls_keyfile', None),
             certfile=config['MQTT'].get('tls_certfile', None),
-            tls_version=ssl.PROTOCOL_SSLv23
+            # Auto-negotiate the highest protocol version that both the client and server support, and configure the
+            # context client-side connections. Other protocol options are deprecated
+            tls_version=ssl.PROTOCOL_TLS_CLIENT
         )
 
     if config['MQTT'].get('username'):
